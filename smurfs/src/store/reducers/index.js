b3bebb0708f79bf396761f39f5e44474866307ce
Min-Huang <minh.huang96@gmail.com>
2019-09-13 08:38:28 -0700

    created app

---
+++ smurfs/src/store/reducers/index.js
@@ -0,0 +1,16 @@
+
+
+const initialState = {
+  name: "",
+    age: null,
+    height: null,
+    id:null,
+};
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    
+    default:
+      return state;
+  }
+};
